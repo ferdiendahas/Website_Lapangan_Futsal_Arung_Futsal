a7531885f2f983c55f9af0da5ed0fca994914a7d
Ferdi Endahas <141556206+ferdiendahas@users.noreply.github.com>
2025-05-16 17:46:10 +0700

    Add files via upload
    Penjadwalan dan booking

---
+++ DataBooking.java
@@ -0,0 +1,19 @@
+package PenjadwalanAndBooking;
+
+public class DataBooking {
+    String nama, tanggal, jam, durasi, lapangan;
+
+    public DataBooking(String nama, String tanggal, String jam, String durasi, String lapangan) {
+        this.nama = nama;
+        this.tanggal = tanggal;
+        this.jam = jam;
+        this.durasi = durasi;
+        this.lapangan = lapangan;
+    }
+
+    public int hitungTotalBayar() {
+        int hargaPerJam = 75000; // contoh harga
+        return Integer.parseInt(durasi) * hargaPerJam;
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,9 @@
+package PenjadwalanAndBooking;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new FormBooking());
+    }
+}
+
