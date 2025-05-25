1244deed3c1a1c63e6b01e00ac8513392fade0b2
Noval Aditya Candra Pratama <143941153+NovalAdityaCP@users.noreply.github.com>
2025-05-26 06:19:54 +0700

    Update Main.java

--- pembayaran&laporan/Main.java
+++ pembayaran&laporan/Main.java
@@ -1,15 +1,12 @@
 package projek;
 
-import static projek.Ekspor_Laporan.exportLaporan;
 import static projek.Rekap_Pendapatan.tampilkanLaporan;
-
-
 
 public class Main {
     
 public static void main(String[] args) {
         tampilkanLaporan();
-        exportLaporan();
+        Ekspor_Laporan.exportPDF();
     }
     
 }
