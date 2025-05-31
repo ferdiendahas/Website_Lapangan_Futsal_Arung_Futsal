d12176b9ade6f2eed51da27256c00c5cab6b802d
Ferdi Endahas <141556206+ferdiendahas@users.noreply.github.com>
2025-05-31 15:21:04 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        // Setup tema FlatLaf
         try {
             com.formdev.flatlaf.FlatIntelliJLaf.setup();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        // Tampilkan login
         javax.swing.SwingUtilities.invokeLater(() -> {
             new LoginFrame().setVisible(true);
         });
