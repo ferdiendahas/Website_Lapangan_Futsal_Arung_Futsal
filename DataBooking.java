535e9d276dddd8fffbb9ea8ea9b6823d617664d3
Dewangga Bintang N <88619792+Dewangguy@users.noreply.github.com>
2025-06-14 00:19:37 +0700

    Update DataBooking.java

--- DataBooking.java
+++ DataBooking.java
@@ -1,16 +1,1 @@
-public class DataBooking {
-    String nama, tanggal, jam, durasi, lapangan;
 
-    public DataBooking(String nama, String tanggal, String jam, String durasi, String lapangan) {
-        this.nama = nama;
-        this.tanggal = tanggal;
-        this.jam = jam;
-        this.durasi = durasi;
-        this.lapangan = lapangan;
-    }
-
-    public int hitungTotalBayar() {
-        int hargaPerJam = 75000;
-        return Integer.parseInt(durasi) * hargaPerJam;
-    }
-}
